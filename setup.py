6c94f5f9e492487d47e9b091549f66c9cf0a1203
Cyril MELTZ <cyril.meltz@campdebases.com>
2022-05-05 15:51:25 +0200

    singer-python==5.9.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-toast',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_toast'],
       install_requires=[
-          'singer-python==5.4.1',
+          'singer-python==5.9.0',
           'requests==2.20.0',
           'backoff==1.3.2',
           'jsonpath_ng==1.5.3'
