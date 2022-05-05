83c59bfe60c6881e4d5d05ba5d3fd00f18682ae2
Cyril MELTZ <cyril.meltz@campdebases.com>
2022-05-05 15:57:50 +0200

    requests==2.21.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-toast',
       py_modules=['tap_toast'],
       install_requires=[
           'singer-python==5.9.0',
-          'requests==2.20.0',
+          'requests==2.21.0',
           'backoff==1.3.2',
           'jsonpath_ng==1.5.3'
       ],
